875cbd9308ab316b21af050acb17866710851ca0
Pengfeizhang0702 <zpf_pengfeizhang@aliyun.com>
2020-07-16 21:20:57 +0800

    网络路径修改

--- baselib/src/main/java/com/zhjy/hdty/work/baselib/Urls.java
+++ baselib/src/main/java/com/zhjy/hdty/work/baselib/Urls.java
@@ -5,7 +5,6 @@ package com.zhjy.hdty.work.baselib;
  * mobile： 969038020@qq.com
  */
 public class Urls {
-
-
-     private  static  final  String baseurl="222222222222222222";
+    private static final String baseurl = "1111111111111111111111";
+    private static final String aaa = "22223232323232";
 }
